27096e76ae829c70ce4390b535ffbda7d89377df
Zenab <zenab.bohra19@gmail.com>
2020-01-22 23:36:18 +1100

    Adds css file in Signin component

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,46 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import styles from './Signin.module.css';
+import Grid from "@material-ui/core/Grid";
 
 export default function SignIn() {
-  const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+      <div className={styles.paper}>
+        <Avatar className={styles.avatar}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={styles.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
@@ -67,14 +47,18 @@ export default function SignIn() {
             type="submit"
             fullWidth
             variant="contained"
-            color="secondary"
-            className={classes.submit}
+            color="primary"
+            className={styles.submit}
           >
             Sign In
           </Button>
-          <Link href="#" variant="body2">
-            {"Don't have an account? Sign Up"}
-          </Link>
+          <Grid container className={styles.link}>
+            <Grid item>
+              <Link href="#" variant="body2" >
+                Don't have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
         </form>
       </div>
     </Container>
